c9717388158a71b2f2e3298f11cb27fc3955944d
Donghyun Kim <eastmon94@naver.com>
2021-04-20 13:21:22 +0900

    move file

--- WebBackend/src/main/java/com/web/ssafy/model/repo/PlaceRepo.java
+++
@@ -1,9 +0,0 @@
-package com.web.ssafy.model.repo;
-
-import com.web.ssafy.model.dto.Place;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlaceRepo extends JpaRepository<Place, Long> {
-}
