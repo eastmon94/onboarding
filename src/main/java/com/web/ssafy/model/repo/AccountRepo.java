c2f74ad0b07540df8df441068019f8dcc48edbfa
Donghyun Kim <eastmon94@naver.com>
2021-04-21 13:36:01 +0900

    remove repository annotation

--- src/main/java/com/web/ssafy/model/repo/AccountRepo.java
+++ src/main/java/com/web/ssafy/model/repo/AccountRepo.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 import com.web.ssafy.model.dto.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-
-@Repository
 public interface AccountRepo extends JpaRepository<Account, Long> {
 
 
--- src/main/java/com/web/ssafy/model/repo/PlaceRepo.java
+++ src/main/java/com/web/ssafy/model/repo/PlaceRepo.java
@@ -2,8 +2,6 @@ package com.web.ssafy.model.repo;
 
 import com.web.ssafy.model.dto.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PlaceRepo extends JpaRepository<Place, Long> {
 }
