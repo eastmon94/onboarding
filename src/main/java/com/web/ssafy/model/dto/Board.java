e279bb4d94488252645f0f6ecf6cc13135e2b0aa
Donghyun Kim <eastmon94@naver.com>
2021-05-03 18:16:16 +0900

    change board

--- src/main/java/com/web/ssafy/model/dto/Board.java
+++ src/main/java/com/web/ssafy/model/dto/Board.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.sql.Date;
+// import java.sql.Date;
 // import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
